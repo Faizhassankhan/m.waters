bc6a581d991d7827c676a59ae70294937e57adb1
Mohammad Farooq <mf.farooq75@gmail.com>
2025-08-04 15:49:48 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,30 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = "https://lxsudjnowijjjjloqubq.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4c3Vkam5vd2lqampqbG9xdWJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDY4MTksImV4cCI6MjA2ODE4MjgxOX0.UwM-hnhGf2Bbh6_Io_putCvYBDzrZSKVbfRFC34Q5aU"
 
-const sessionStorageAdapter = {
-  getItem: (key: string) => {
-    if (typeof sessionStorage === 'undefined') return null;
-    return sessionStorage.getItem(key);
-  },
-  setItem: (key: string, value: string) => {
-    if (typeof sessionStorage === 'undefined') return;
-    sessionStorage.setItem(key, value);
-  },
-  removeItem: (key: string) => {
-    if (typeof sessionStorage === 'undefined') return;
-    sessionStorage.removeItem(key);
-  },
-};
-
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-        storage: sessionStorageAdapter,
-        autoRefreshToken: true,
-        persistSession: false, // This is set to false to not persist session across tabs/windows
-        detectSessionInUrl: true,
-    },
-});
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
