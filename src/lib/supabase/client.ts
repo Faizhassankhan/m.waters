a8fdde8b32480d124ad99887450ab9cc382d0bd1
Mohammad Farooq <mf.farooq75@gmail.com>
2025-08-06 10:55:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -4,4 +4,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = "https://lxsudjnowijjjjloqubq.supabase.co"
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4c3Vkam5vd2lqampqbG9xdWJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDY4MTksImV4cCI6MjA2ODE4MjgxOX0.UwM-hnhGf2Bbh6_Io_putCvYBDzrZSKVbfRFC34Q5aU"
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
+});
