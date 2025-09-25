e2d5b68490fcb2f73ee14d09186493c2d5bd36db
Mohammad Farooq <mf.farooq75@gmail.com>
2025-09-25 10:56:38 +0000

    acha ye supabse ki keys lagado
    NEXT_PUBLIC_SUPABASE_URL=https://mmhwfzt

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://lxsudjnowijjjjloqubq.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4c3Vkam5vd2lqampqbG9xdWJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDY4MTksImV4cCI6MjA2ODE4MjgxOX0.UwM-hnhGf2Bbh6_Io_putCvYBDzrZSKVbfRFC34Q5aU"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
