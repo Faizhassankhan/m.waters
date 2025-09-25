a6cb985b91c0a96f37436d124ecc33e0cc5cbd7b
Mohammad Farooq <mf.farooq75@gmail.com>
2025-09-25 10:07:23 +0000

    acha to iske liye customer login bhi rehna chaiye apne account me take u

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -6,6 +6,7 @@ const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYm
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
+    storage: typeof window !== 'undefined' ? window.localStorage : undefined,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true,
